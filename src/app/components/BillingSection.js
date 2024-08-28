f0be03cc026204db05a9bc08ff451b0cd4de863a
Evans Liaudet <evans.liaudet@crea-inseec.com>
2024-08-28 11:38:06 +0200

    Responsive on desktop and tablet

--- src/app/components/BillingSection.js
+++ src/app/components/BillingSection.js
@@ -1,96 +1,132 @@
 "use client";
 
 import { useState } from "react";
-import Input from "./Input";
+import "./Billing.css";
 
 export default function BillingSection() {
   const [error, setError] = useState(false);
   const [cvvError, setCvvError] = useState(false);
 
-  const handleCVV = (e) => {
-    setCvvError(false);
-    if (e.target.value.length > 3) {
-      setCvvError(true);
-    }
-  };
-
   return (
-    <section className="py-12 mx-4">
+    <section className="py-12 mx-4 md:mx-10 md:py-20 lg:mx-32">
       <div className="pb-8">
         <h1 className="font-medium text-2xl">Billing Information</h1>
         <p className="text-neutral-500">
           Update your billing details and address
         </p>
       </div>
-      <div className="flex flex-col gap-5">
+      <div className="flex flex-col gap-5 md:flex-row md:justify-between">
         <p className="text-lg font-medium">Payment details</p>
-        <div className="flex flex-col gap-7">
-          <Input
-            title={"Card number"}
-            inputVal={"1234 1234 1234 1234"}
-            inputType={"number"}
-          />
-          <Input
-            title={"Cardholder name"}
-            inputVal={"Full name on card"}
-            inputType={"number"}
-          />
+        <div className="flex flex-col gap-7 md:w-2/3">
+          <div className="flex flex-col gap-2">
+            <p className="">Card number</p>
+            <input
+              type="number"
+              placeholder="1234 1234 1234 1234"
+              className={`bg-neutral-100 rounded px-5 py-2 w-full placeholder:text-neutral-500 border border-neutral-200 `}
+            ></input>
+          </div>
+          <div className="flex flex-col gap-2">
+            <p className="">Cardholder name</p>
+            <input
+              type="text"
+              placeholder="Full name on card"
+              className={`bg-neutral-100 rounded px-5 py-2 w-full placeholder:text-neutral-500 border border-neutral-200 `}
+            ></input>
+          </div>
           <div className="flex gap-8">
-            <div className="">
-              <Input title={"Expiry"} inputVal={"MM/YY"} inputType={"number"} />
+            <div className="flex flex-col gap-2 w-1/2">
+              <p className="">Expiry</p>
+              <input
+                type="date"
+                placeholder="MM/YY"
+                className={`bg-neutral-100 rounded px-5 py-2 w-full placeholder:text-neutral-500 border border-neutral-200 `}
+              ></input>
             </div>
-            <div className="">
-              <Input
-                title={"CVV"}
-                inputVal={"123"}
-                inputType={"number"}
-                func={handleCVV}
-              />
-              {cvvError && <p className="text-red-500">CVV format invalid</p>}
+            <div className="flex flex-col gap-2 w-1/2">
+              <p className="">CVV</p>
+              <input
+                type="number"
+                placeholder="123"
+                className={`bg-neutral-100 rounded px-5 py-2 w-full placeholder:text-neutral-500 border border-neutral-200 `}
+                onChange={(e) => {
+                  if (e.target.value.length > 3) {
+                    console.log("test");
+                  }
+                }}
+              ></input>
             </div>
           </div>
         </div>
       </div>
       <hr className="my-5"></hr>
-      <div className="flex flex-col gap-5">
+      <div className="flex flex-col gap-5 md:flex-row md:justify-between">
         <p className="">Email address</p>
-        <div className="">
-          <Input
-            title={"Email"}
-            inputVal={"user@example.com"}
-            inputType={"email"}
-          />
+        <div className="flex flex-col gap-2 md:w-2/3">
+          <p className="">Email</p>
+          <input
+            type="number"
+            placeholder="user@example.com"
+            className={`bg-neutral-100 rounded px-5 py-2 w-full placeholder:text-neutral-500 border border-neutral-200 `}
+          ></input>
         </div>
       </div>
       <hr className="my-5"></hr>
-      <div className="flex flex-col gap-5">
+      <div className="flex flex-col gap-5 md:flex-row md:justify-between">
         <p className="">Address details</p>
-        <Input
-          title={"Country / Region"}
-          disabled={true}
-          inputVal={"United States"}
-        />
-        <div className="flex flex-col gap-2">
-          <Input
-            title={"Address"}
-            inputVal={"Street address"}
-            inputType={"text"}
-          />
-          <Input
-            inputVal={"Apartment, suite, etc (optional)"}
-            inputType={"text"}
-          />
-        </div>
-        <div className="flex items-end gap-5">
-          <Input title={"City"} inputVal={"City"} inputType={"text"} />
-          <select
-            className="bg-neutral-100 rounded px-5 py-2 w-full placeholder:text-neutral-500 border border-neutral-200 h-[42px]"
-            name=""
-            placeholde="State"
-          >
-            <option value="" className=""></option>
-          </select>
-          <Input title={"Zip"} inputVal={"Zip"} inputType={"number"} />
+        <div className="flex flex-col gap-7 md:w-2/3">
+          <div className="flex flex-col gap-2">
+            <p className="">Country / Region</p>
+            <input
+              type="text"
+              placeholder="United States"
+              className={`bg-neutral-100 rounded px-5 py-2 w-full placeholder:text-neutral-500 border border-neutral-200 `}
+              disabled="true"
+            ></input>
+          </div>
+          <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-2">
+              <p className="">Address</p>
+              <input
+                type="text"
+                placeholder="Street address"
+                className={`bg-neutral-100 rounded px-5 py-2 w-full placeholder:text-neutral-500 border border-neutral-200 `}
+              ></input>
+            </div>
+            <div className="flex flex-col gap-2">
+              <input
+                type="text"
+                placeholder="Apartment, suite, etc (optional)"
+                className={`bg-neutral-100 rounded px-5 py-2 w-full placeholder:text-neutral-500 border border-neutral-200 `}
+              ></input>
+            </div>
+          </div>
+          <div className="flex items-end gap-5">
+            <div className="flex flex-col gap-2">
+              <p className="">City</p>
+              <input
+                type="text"
+                placeholder="City"
+                className={`bg-neutral-100 rounded px-5 py-2 w-full placeholder:text-neutral-500 border border-neutral-200 `}
+              ></input>
+            </div>
+            <select
+              className="bg-neutral-100 rounded px-5 py-2 w-full placeholder:text-neutral-500 border border-neutral-200 h-[42px]"
+              name=""
+            >
+              <option value="State" className="">
+                State
+              </option>
+            </select>
+            <div className="flex flex-col gap-2">
+              <p className="">Zip</p>
+              <input
+                type="number"
+                placeholder="123"
+                className={`bg-neutral-100 rounded px-5 py-2 w-full placeholder:text-neutral-500 border border-neutral-200 `}
+              ></input>
+            </div>
+          </div>
         </div>
       </div>
       <div className="flex justify-end pt-5">
